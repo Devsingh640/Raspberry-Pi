0e0bcf55339d1bb321ea23319f949e4173ccc7fa
Om Dev Singh <devsingh640@gmail.com>
2017-12-02 05:00:26 +0700

    Update Setting_GPIO_Pin_(high,low).py

--- Python_With_Raspberry_Pi/Setting_GPIO_Pin_(high,low).py
+++ Python_With_Raspberry_Pi/Setting_GPIO_Pin_(high,low).py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO    # Loading the GPIO library.
-import time
+import time                # Loading the time library.
 
 GPIO.setmode(GPIO.BOARD)   
 
 GPIO.setup(10,GPIO.OUT)    # Setting the pin in output mode, current will flow out from this pin.
-GPIO.output(10,TRUE)       # setting the pin high (5v). 
+GPIO.output(10,TRUE)       # Setting the pin high (5v). 
 GPIO.output(11,1)
-GPIO.output(10,FALSE)      # setting the pin low (0v). 
+GPIO.output(10,FALSE)      # Setting the pin low (0v). 
 GPIO.output(11,0)
 GPIO.cleanup()             # Will take back the pin.

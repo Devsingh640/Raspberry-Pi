ca9730c59e0c327617410e5d9ec0f96ab0e5650e
Om Dev Singh <devsingh640@gmail.com>
2019-06-17 22:26:21 +0530

    Update Blinky_led.py

--- Python_With_Raspberry_Pi/Blinky_led.py
+++ Python_With_Raspberry_Pi/Blinky_led.py
@@ -1,1 +1,7 @@
+import time                             #This will import time library that can be used to set delay.
+import RPi.GPIO as GPIO                 #This will allow you to use general purpose input output pins.
+red = 17                                #This will assign pin 17 to variable named red.
 
+GPIO.setup(red, OUT)                    #This will set mode of pin 17 that is stored in variable red to out.
+
+GPIO.cleanup                            #This will release the issued pins ie pin will be send back to the unused pin domain and now can be called in different program.
